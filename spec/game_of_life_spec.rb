f5810c4b20de17d2c40061f4a176881b93ff0926
Nitish Rathi <nitishrathi@gmail.com>
2012-03-07 14:09:32 +0530

    grid with one live cell

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -3,11 +3,19 @@ describe "In the next gen, a grid with" do
     it "should have no live cells" do
       g = []
       ng = next_gen(g)
-      ng.should == g
+      ng.should == []
+    end
+  end
+
+  context "one live cell" do
+    it "should have no live cells" do
+      g = [[0,0]]
+      ng = next_gen(g)
+      ng.should == []
     end
   end
 
   def next_gen(grid)
-    grid
+    []
   end
 end
