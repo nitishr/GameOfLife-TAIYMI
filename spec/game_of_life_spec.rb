22ed5041d0a89cbc6bac6df8b6848804538a0691
Nitish Rathi <nitishrathi@gmail.com>
2012-03-07 14:07:13 +0530

    extract method next_gen

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -2,8 +2,12 @@ describe "In the next gen, a grid with" do
   context "no live cells" do
     it "should have no live cells" do
       g = []
-      ng = g
+      ng = next_gen(g)
       ng.should == g
     end
+  end
+
+  def next_gen(grid)
+    grid
   end
 end
